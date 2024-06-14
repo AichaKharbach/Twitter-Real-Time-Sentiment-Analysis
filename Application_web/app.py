125ba41e0c783c0ee0867f1780bbc5fd259bc965
Aicha Kharbach <96926505+AichaKharbach@users.noreply.github.com>
2024-06-14 18:26:15 +0100

    Add files via upload

--- Application_web/app.py
+++ Application_web/app.py
@@ -3,8 +3,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Connection to MongoDB
-client = MongoClient("mongodb://host.docker.internal:27017")
+client = MongoClient("mongodb://localhost:27017/")
 db = client["twitter_sentiment_db"]
 collection = db["predictions"]
 
@@ -19,15 +18,33 @@ def map_prediction(prediction):
     return mapping.get(prediction, "Unknown")
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/predictions')
 def get_predictions():
-    predictions = list(collection.find({}, {'_id': False}))
+    predictions = list(collection.find({}, {"_id": 0}))
     for prediction in predictions:
         prediction["prediction"] = map_prediction(prediction["prediction"])
     return jsonify(predictions)
 
-if __name__ == "__main__":
+@app.route('/chart-data')
+def get_chart_data():
+    pipeline = [
+        {"$group": {"_id": "$prediction", "count": {"$sum": 1}}}
+    ]
+    results = list(collection.aggregate(pipeline))
+    sentiment_data = {map_prediction(item["_id"]): item["count"] for item in results}
+
+    chart_data = {
+        "labels": list(sentiment_data.keys()),
+        "data": list(sentiment_data.values())
+    }
+    return jsonify(chart_data)
+
+if __name__ == '__main__':
     app.run(debug=True)
